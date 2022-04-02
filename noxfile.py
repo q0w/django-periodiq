1c66ea9b182720491cec89f88e465ebd8e67606b
q0w <43147888+q0w@users.noreply.github.com>
2022-04-02 19:20:21 +0300

    Add GHA

--- noxfile.py
+++ noxfile.py
@@ -1,9 +1,20 @@
 import nox
 
+nox.options.reuse_existing_virtualenvs = True
+nox.options.sessions = ["lint"]
 
-@nox.session(python=["3.7", "3.8", "3.9"])
-@nox.parametrize("django", ["2.2", "3.0", "3.2"])
-def tests(session, django):
+
+@nox.session
+@nox.parametrize(
+    "python,django",
+    [
+        (python, django)
+        for python in ("3.7", "3.8", "3.9", "3.10")
+        for django in ("2.2", "3.0", "3.2", "4.0")
+        if (python, django) != ("3.7", "4.0")
+    ],
+)
+def test(session: nox.Session, django: str) -> None:
     session.install("-r", "requirements-dev.txt")
     session.install(f"django=={django}")
     session.run("coverage", "erase")
@@ -12,7 +23,12 @@ def tests(session, django):
     session.run("coverage", "report")
 
 
-@nox.session()
-def lint(session):
+@nox.session
+def lint(session: nox.Session) -> None:
     session.install("pre-commit")
-    session.run("pre-commit", "run", "--all-files", "--show-diff-on-failure")
+    if session.posargs:
+        args = session.posargs + ["--all-files"]
+    else:
+        args = ["--all-files", "--show-diff-on-failure"]
+
+    session.run("pre-commit", "run", *args)
